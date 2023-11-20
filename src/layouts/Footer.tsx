24553356ba575c20a61f90d1276e213325d03901
uniS2 <s2oie1010@gmail.com>
2023-11-20 16:24:32 +0900

    Init: 파일 기본 구조 추가 - assets 추가 - Header, Footer 컴포넌트 추가 - TS 선언 파일 추가

---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Footer() {
+  return <footer>Twelive</footer>;
+}
+
+export default Footer;
---
+++ src/layouts/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Header() {
+  return <header>Twelive</header>;
+}
+
+export default Header;
