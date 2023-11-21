3ce65962596df85daaa323e78117a0fd9362a730
Jeon SunYong <sunyong0725@naver.com>
2023-11-21 14:52:26 +0900

    Bug : merge 중 충돌 발생 충돌 발생 후 수정 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
 import GlobalStyles from './GlobalStyles';
 
-=======
-import Logo from '@assets/common-logo.svg';
->>>>>>> 47a7a759719c53b8f4d26d8e33a257b1a3f278e1
 function App() {
   return (
     <>
