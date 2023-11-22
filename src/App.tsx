85424ededc836c7fd7aa7cef054514b99cd4c090
uniS2 <s2oie1010@gmail.com>
2023-11-22 10:51:42 +0900

    Style: `styled-components`의 ThemeProvider를 이용한 반응현 구현 - `theme.ts`: theme 변수로 미디어쿼리 중단점을 설정하였습니다. - App 컴포넌트 내 `ThemeProvider`: props로 theme 변수를 넘겨주어 반응형으로 구현될 수 있도록 하였습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import GlobalStyles from './GlobalStyles';
+import NavBar from './layouts/NavBar';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
 
 function App() {
   return (
     <>
       <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <NavBar />
+      </ThemeProvider>
     </>
   );
 }
