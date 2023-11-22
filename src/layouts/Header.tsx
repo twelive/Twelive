e8b13fc9bc7d735d32bf73c1bd8ba8850839e626
MyoungHwaShin <shinmyounghwa92@gmail.com>
2023-11-22 16:29:49 +0900

    Revert "Revert "Feat:Header 컴포넌트 반응형 적용 테스트 Fixes""
    This reverts commit 96cb14b0785517aa9300c3f53c0b43934f37e542.

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,9 +1,20 @@
 import React, { ReactElement } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import styled, { ThemeProvider } from "styled-components";
 import mainlogo from '../assets/common-logo.svg';
 import search from '../assets/common-search.svg';
 import login from '../assets/common-login.svg';
+import menu from '../assets/common-menu.svg';
+import mic from '../assets/common-mic.svg'
 import theme from '../../src/theme';
+import HeaderButton from '../components/HeaderButton';
+
+
+
+
+
+
+
 
 
 interface Head {
@@ -15,25 +26,58 @@ alt: string;
 }
 
 function Header(): ReactElement {
+
+  const isMobile = useMediaQuery ({
+    query : "(min-width : 20rem) and (max-width : 47.9375rem)"
+  })
+  const isLaptop = useMediaQuery ({
+    query : "(min-width : 48rem) and (max-width : 63.125rem)"
+  })
+  const isDesktop = useMediaQuery ({
+    query : "(min-width : 63.1875rem) and (max-width : 120rem)"
+  })
+
   return (  <>
   
   <HeadBar>
+
     <Div className='buttonContainer'>
-  <Button className="homeButton">
-    <img src={mainlogo} alt="트웰브 홈으로" className="logo"/>
-  </Button>
+      {isMobile && <HeaderButton buttonClass="homeButton" src={mainlogo} title="트웰브 홈으로">
+
+  </HeaderButton>}
+  {(isLaptop || isDesktop) && (<div className ='leftContainer'>  <HeaderButton buttonClass="hamburger" src={menu} title="상세 메뉴">
+
+</HeaderButton> <HeaderButton buttonClass="homeButton" src={mainlogo} title="트웰브 홈으로">
+
+</HeaderButton> </div>)}
   
-  <Div>
-    
-  <Button className="searchButton">
-    <img src={search} alt="검색버튼" className="search"/>
-  </Button>
+  
+  
+  {isMobile &&  <div><HeaderButton buttonClass="searchButton" src={search} title="검색버튼"> </HeaderButton> <HeaderButton buttonClass="loginButton" src={login} title="사용자 ">
+  </HeaderButton></div>}
 
-  <Button className="loginButton">
-    <img src={login} alt="사용자 " className="userlo"/>
-  </Button>
+  {(isLaptop || isDesktop) && ( <div className='searchWrapper'>
+    <div className='searchContainer'><form id="form" name="form" action="" method="get">
+            <div className="search-wrapper">
+                <fieldset className="search">
+                    <input type="text" className="searchTxt" name="searchTxt" title="영상검색" placeholder="검색"/>
+                </fieldset>
+            </div>
+</form>
+<HeaderButton buttonClass='searchButton' src={search} title='영상검색' ></HeaderButton>
 
-  </Div>
+      </div> <HeaderButton buttonClass='searchButton' src={mic} title='음성검색' ></HeaderButton>
+  </div>)}
+  
+
+
+  {(isLaptop || isDesktop) && (<div>
+    <HeaderButton buttonClass="loginButton" src={login} title="사용자"><span>로그인</span>
+  </HeaderButton>
+  </div>)}
+  
+
+
 
 
     </Div>
@@ -58,67 +102,6 @@ box-shadow: 1px 1px 4px 0px #D4D4D4;
 
 `;
 
-const Button = styled.button`
-border-style: none;
-box-sizing: border-box;
-height: 1.5rem;
-background-color: transparent;
-cursor: pointer;
-
-
-
-@media ${(props) => props.theme.mobile} {
-  &.homeButton {
-    background-color: red;
-  
-    height: 1.25rem;
-  
-  }
-  
-  &.searchButton {
-    height: 1.5rem;
-  }
-  
-  
-  &.loginButton {
-    height: 1.5rem;
-  }
-
-};
-@media ${(props) => props.theme.tablet} {
-  &.homeButton {
-    background-color: blue;
-    
-    height: 1.25rem;
-  
-  };
-  
-  &.searchButton {
-    height: 1.5rem;
-  };
-  
-  
-  &.loginButton {
-    height: 1.5rem;
-  };
-};
-@media ${(props) => props.theme.laptop} {
-  &.homeButton {
-    background-color: black;
-    height: 1.25rem;
-  
-  };
-  
-  &.searchButton {
-    height: 1.5rem;
-  };
-  
-  
-  &.loginButton {
-    height: 1.5rem;
-  };
-};
-`;
 
 const Div = styled.div `
 &.buttonContainer {
@@ -128,6 +111,11 @@ const Div = styled.div `
   transform: translate(0, 50%);
 
     
+
+}
+
+&.searchWrapper {
+  
 
 }
 
