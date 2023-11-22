bd2510299d47389c14eff47fee91d9551eb31954
uniS2 <s2oie1010@gmail.com>
2023-11-22 15:22:43 +0900

    Feat: `Provider`로 props `store` 지정한 통합 관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import GlobalStyles from './GlobalStyles';
+import GlobalStyles from './styles/GlobalStyles';
 import NavBar from './layouts/NavBar';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import { ThemeProvider } from 'styled-components';
-import theme from './theme';
+import theme from './styles/theme';
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <ThemeProvider theme={theme}>
-        <NavBar />
+        <Provider store={store}>
+          <NavBar />
+        </Provider>
       </ThemeProvider>
     </>
   );
