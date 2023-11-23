f12d0a76912ca3bc6f1d093c2351c270d90f0e02
Jeon SunYong <sunyong0725@naver.com>
2023-11-23 09:23:42 +0900

    Init : index.d.ts 타입 지정 완료 popular.json 에 있는 타입을 interface 화 완료했습니다. 데이터 불러오실때 타입확인하시고 불러오시면 좋을것같습니다.

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -11,3 +11,84 @@ type NavAction = {
   type: 'click';
   payload?: string;
 };
+
+// popular.json 타입 정의
+
+export interface VideoSnippet {
+  categoryId: string;
+  channelId: string;
+  channelTitle: string;
+  defaultAudioLanguage: string;
+  defaultLanguage: string;
+  description: string;
+  liveBroadcastContent: string;
+  localized: {
+    description: string;
+    title: string;
+  };
+  publishedAt: string;
+  tags: string[];
+  thumbnails: {
+    default: {
+      height: number;
+      url: string;
+      width: number;
+    };
+    high: {
+      height: number;
+      url: string;
+      width: number;
+    };
+    maxres: {
+      height: number;
+      url: string;
+      width: number;
+    };
+    medium: {
+      height: number;
+      url: string;
+      width: number;
+    };
+    standard: {
+      height: number;
+      url: string;
+      width: number;
+    };
+  };
+  title: string;
+}
+
+export interface VideoItem {
+  etag: string;
+  id: string;
+  kind: string;
+  snippet: VideoSnippet;
+}
+
+export interface RelatedVideoItem {
+  etag: string;
+  id: RelatedId;
+  kind: string;
+  snippet: VideoSnippet;
+}
+
+export interface RelatedId {
+  kind: string;
+  videoId: string;
+}
+
+export interface VideoListResponse {
+  etag: string;
+  items: VideoItem[];
+  kind: string;
+  nextPageToken: string;
+  pageInfo: {
+    resultsPerPage: number;
+    totalResults: number;
+  };
+}
+
+export interface ThumbnailImg {
+  $image?: string;
+  $height?: number;
+}
