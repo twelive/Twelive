65e1d1e4ba8c147921adba7446cd180fbeb4c3a6
seojinman <dirnrhd21@naver.com>
2023-11-23 13:04:50 +0900

    Feat: 메뉴 버튼 클릭시 메뉴바 생성

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import NavBar from './layouts/NavBar';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 import Header from './layouts/Header';
+import Menubar from './layouts/MenuBar';
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <ThemeProvider theme={theme}>
+        <Menubar/>
         <Header/>
         <NavBar />
       </ThemeProvider>
