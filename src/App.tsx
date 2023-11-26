4fc7cc3679c5785c122a087fa1c72eb68e52cdfe
MyoungHwaShin <shinmyounghwa92@gmail.com>
2023-11-26 19:26:58 +0900

    Feat: App.tsx에서 Provider 제거 후 현재 반영용 ThmeProvider 추가
    App.tsx에서 Provider 제거 후 root인 index로 옮겨 다크모드 적용했습니다.  현재 테마 실시간 반영용 ThmeProvider로 변경하였습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import GlobalStyles from './GlobalStyles';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 import Router from './Router';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const darkMode = useSelector((state: RootState) => state.darkMode.darkMode);
+  const currentTheme = {
+    ...theme.device,
+    ...(darkMode ? theme.darkMode : theme.lightMode)
+  };
   return (
     <>
+      <ThemeProvider theme={currentTheme}>
       <GlobalStyles />
-      <ThemeProvider theme={theme}>
         <Router />
       </ThemeProvider>
     </>
