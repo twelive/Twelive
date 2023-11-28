179c385cb2747b72cada06b820dd2112cfa50859
uniS2 <s2oie1010@gmail.com>
2023-11-28 15:50:02 +0900

    Feat: 검색 data 저장 Reducer 추가 - `searchReducer`에서 전역적으로 상태를 관리하여 input 이벤트가 일어나면 state 값을 변경합니다. - 관련 타입과 searchReducer를 사용할 수 있도록 rootReducer에 추가해주었습니다.

---
+++ src/reducers/searchReducer.ts
@@ -0,0 +1,21 @@
+const initialState: SearchState = {
+  searchValue: '',
+};
+
+function searchReducer(
+  state: SearchState = initialState,
+  action: SearchAction
+) {
+  switch (action.type) {
+    case 'SEARCH_INPUT': {
+      return {
+        ...state,
+        searchValue: action.value || '',
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+export default searchReducer;
