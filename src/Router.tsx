585277c583acf1102e9c70660c299b5c4694fa7d
uniS2 <s2oie1010@gmail.com>
2023-11-28 15:47:11 +0900

    Feat: 검색 페이지 경로 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RootLayout from '@/RootLayout';
 import MainListPage from '@pages/MainListPage';
 import DetailPage from '@pages/DetailPage';
+import SearchPage from '@pages/SearchPage';
 import Error404Page from '@pages/Error404Page';
 
 const Router = () => {
@@ -12,6 +13,7 @@ const Router = () => {
         <Route element={<RootLayout />}>
           <Route path="/" element={<MainListPage />} />
           <Route path="/detail/:detailId" element={<DetailPage />} />
+          <Route path="/search" element={<SearchPage />} />
           <Route path="*" element={<Error404Page />} />
         </Route>
       </Routes>
