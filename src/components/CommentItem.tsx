4237ba725c2ce45069e5fff0b27e1ea0fa9a941a
Jeon SunYong <sunyong0725@naver.com>
2023-11-29 09:46:23 +0900

    Refactor : CommentListWrapper tablet 높이 수정 CommentListWrapper tablet 높이 수정

--- src/components/CommentItem.tsx
+++ src/components/CommentItem.tsx
@@ -4,23 +4,20 @@ import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import profile from '../assets/장원영쟝.jpg';
 import sort from '../assets/detail-sort.svg';
-import whitesort from '../assets/detail-whitesort.svg'
+import whitesort from '../assets/detail-whitesort.svg';
 
 type Comment = {
   id: number;
   text: string;
   video_id: string;
   created_at: string;
-
 };
 
-
-function CommentItem () {
+function CommentItem() {
   const images = {
     darkMode: {
       sort: whitesort,
-
-        },
+    },
     lightMode: {
       sort: sort,
     },
@@ -35,9 +32,7 @@ function CommentItem () {
   const handleCommentSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-
     if (commentInputRef.current) {
-
       const commentText = commentInputRef.current.value;
       if (commentText) {
         const { data, error } = await supabase
@@ -48,11 +43,10 @@ function CommentItem () {
           console.error('Error adding comment:', error);
         } else {
           setComments([data[0], ...comments]);
-          commentInputRef.current.value = ''; 
+          commentInputRef.current.value = '';
         }
       }
     }
-  
   };
 
   const handleEdit = async (id: number) => {
@@ -84,7 +78,7 @@ function CommentItem () {
       }
     }
   };
-  
+
   const handleDelete = async (id: number) => {
     try {
       const { error } = await supabase.from(TABLE_NAME).delete().eq('id', id);
@@ -101,12 +95,11 @@ function CommentItem () {
   useEffect(() => {
     const fetchComments = async () => {
       const { data, error } = await supabase
-  .from(TABLE_NAME)
-  .select('id, text, video_id, created_at') 
-  .eq('video_id', channelId)
-  .order('created_at', { ascending: false });
+        .from(TABLE_NAME)
+        .select('id, text, video_id, created_at')
+        .eq('video_id', channelId)
+        .order('created_at', { ascending: false });
 
-  
       if (error) {
         console.error('Error fetching comments:', error);
       } else {
@@ -114,89 +107,74 @@ function CommentItem () {
       }
     };
 
-  
-  
     fetchComments();
-
-    
-
-
-
   }, [channelId]);
   return (
     <CommentContainer>
-
-    <CommentInfo>
-                
-                <CommentsCounter>댓글{comments.length}개</CommentsCounter>
-                <CommentsSort>
-                  <SortIcon src={currentImages.sort}></SortIcon>
-                  <SortBy>분류</SortBy>
-                </CommentsSort>
-              </CommentInfo>
-    <CommentWrapper>
-                <CommentAdd>
-                  <CommentFormArea>
+      <CommentInfo>
+        <CommentsCounter>댓글{comments.length}개</CommentsCounter>
+        <CommentsSort>
+          <SortIcon src={currentImages.sort}></SortIcon>
+          <SortBy>분류</SortBy>
+        </CommentsSort>
+      </CommentInfo>
+      <CommentWrapper>
+        <CommentAdd>
+          <CommentFormArea>
+            <Commenter src={profile}></Commenter>
+            <CommentForm id="comment" onSubmit={handleCommentSubmit}>
+              <CommentInput
+                type="text"
+                placeholder="댓글 내용을 입력해주세요."
+                name="content"
+                required
+                ref={commentInputRef}
+              />
+            </CommentForm>
+            <CommentButton type="submit" form="comment">
+              <CommentButtonText>답글</CommentButtonText>
+            </CommentButton>
+          </CommentFormArea>
+          <CommentListWrapper>
+            {comments.map((comment) => (
+              <CommentList key={comment.id}>
+                <CommenterBox>
                   <Commenter src={profile}></Commenter>
-                    <CommentForm id='comment' onSubmit={handleCommentSubmit}>
-                      <CommentInput
-                        type="text"
-                        placeholder="댓글 내용을 입력해주세요."
-                        name="content"
-                        required
-                        ref={commentInputRef}
-                      />
-                    </CommentForm >
-                      <CommentButton type='submit' form='comment'><CommentButtonText>답글</CommentButtonText></CommentButton>
-
-                      
-                  </CommentFormArea>
-                  <CommentListWrapper>
-                  {comments.map((comment) => (
-                   
-             
-                   <CommentList key={comment.id}>
-                     <CommenterBox>
-               
-                     <Commenter src={profile}></Commenter>       
-                     {comment.id === editingCommentId ? (
-                       <EditInput
-                         key={comment.id}
-                         type="text"
-                         value={newCommentText}
-                         onChange={(e) => setNewCommentText(e.target.value)}
-                       />
-                     ) : (
-                       <CommentText key={comment.id}>{comment.text}</CommentText>
-                     )}
-                     </CommenterBox>
-                     <CommentButtonContainer>
-                       <UpdatedAt>{comment.created_at.slice(2,10)}</UpdatedAt>
-                     <CommentEdit onClick={()=>handleEdit(comment.id)}><CommentButtonText>수정</CommentButtonText></CommentEdit>
-                     <CommentDelete onClick={() => handleDelete(comment.id)}><CommentButtonText>삭제</CommentButtonText></CommentDelete>
-                     </CommentButtonContainer>
-               
-                   </CommentList>
-                 ))}
-                  </CommentListWrapper>
-                  
-
-                </CommentAdd>
-              </CommentWrapper>
-            </CommentContainer>
-
+                  {comment.id === editingCommentId ? (
+                    <EditInput
+                      key={comment.id}
+                      type="text"
+                      value={newCommentText}
+                      onChange={(e) => setNewCommentText(e.target.value)}
+                    />
+                  ) : (
+                    <CommentText key={comment.id}>{comment.text}</CommentText>
+                  )}
+                </CommenterBox>
+                <CommentButtonContainer>
+                  <UpdatedAt>{comment.created_at.slice(2, 10)}</UpdatedAt>
+                  <CommentEdit onClick={() => handleEdit(comment.id)}>
+                    <CommentButtonText>수정</CommentButtonText>
+                  </CommentEdit>
+                  <CommentDelete onClick={() => handleDelete(comment.id)}>
+                    <CommentButtonText>삭제</CommentButtonText>
+                  </CommentDelete>
+                </CommentButtonContainer>
+              </CommentList>
+            ))}
+          </CommentListWrapper>
+        </CommentAdd>
+      </CommentWrapper>
+    </CommentContainer>
   );
-
 }
 
-export default CommentItem
+export default CommentItem;
 
-const CommentContainer = styled.div `
+const CommentContainer = styled.div``;
 
-`;
-
-const CommentInfo = styled.div `
-display: flex;
+const CommentInfo = styled.div`
+  display: flex;
   flex-direction: row;
   align-items: center;
   vertical-align: middle;
@@ -205,16 +183,11 @@ display: flex;
   background-color: var(--darkmode-bgColor);
   gap: 50px;
   font-size: 20px;
-  
-
 `;
-const CommentsCounter = styled.h1 `
-  
-`;
+const CommentsCounter = styled.h1``;
 
-
-const CommentsSort = styled.button `
-display: flex;
+const CommentsSort = styled.button`
+  display: flex;
   flex-direction: row;
   align-items: center;
   vertical-align: middle;
@@ -225,92 +198,74 @@ display: flex;
   padding: 2px;
   border-radius: 10%;
   &:hover {
-    background-color: ${(props) => props.theme.hoverColor};;
+    background-color: ${(props) => props.theme.hoverColor};
   }
 `;
-const SortIcon = styled.img `
+const SortIcon = styled.img``;
+const SortBy = styled.p``;
 
-`;
-const SortBy = styled.p `
-  
-`;
-
-
-const Commenter = styled.img `
+const Commenter = styled.img`
   width: 24px;
   height: 24px;
   border-radius: 100%;
- 
 `;
 
-const CommentWrapper = styled.div `
+const CommentWrapper = styled.div``;
 
-`;
-
-const CommentAdd = styled.ul `
-
-`;
-const CommentFormArea = styled.div `
-
-display: flex;
+const CommentAdd = styled.ul``;
+const CommentFormArea = styled.div`
+  display: flex;
   flex-direction: row;
   align-items: center;
   vertical-align: middle;
   justify-content: space-between;
   padding-top: 15px;
   margin-bottom: 20px;
-  
 `;
-const CommentForm = styled.form `
-width: 100%;
-display: flex;
+const CommentForm = styled.form`
+  width: 100%;
+  display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
 `;
-const CommentInput = styled.input `
-width: 90%;
-background-color: transparent;
-border: none;
-border-bottom: 1px solid;
-color: var(--darkmode-color);
-font-size: 18px;
-margin-left: 30px;
-margin-right: 30px;
-padding: 5px;
-&:focus {
+const CommentInput = styled.input`
+  width: 90%;
+  background-color: transparent;
+  border: none;
+  border-bottom: 1px solid;
+  color: var(--darkmode-color);
+  font-size: 18px;
+  margin-left: 30px;
+  margin-right: 30px;
+  padding: 5px;
+  &:focus {
+    background-color: #a3a3a33e;
+  }
+`;
+
+const CommentButton = styled.button`
+  width: 40px;
+  height: 24px;
+  border: 0.1px solid var(--button-border-color);
+  border-radius: 5px;
+  background-color: var(--darkmode-bgColor);
+  &:hover {
+    background-color: ${(props) => props.theme.hoverColor};
+  }
+`;
+const CommentList = styled.li`
   background-color: #a3a3a33e;
-}
+  margin: 5px;
+  margin-bottom: 20px;
+
+  border-radius: 10px;
+  padding: 15px;
+  font-size: 15px;
+  line-height: 20px;
 `;
 
-
-
-const CommentButton = styled.button `
-width: 40px;
-height: 24px;
-border: 0.1px solid var(--button-border-color);
-border-radius: 5px;
-background-color: var(--darkmode-bgColor);
-&:hover {
-  background-color: ${(props) => props.theme.hoverColor};
-
-
-}
-
-
-`;
-const CommentList = styled.li `
-background-color: #a3a3a33e;
-margin: 5px;
-margin-bottom: 20px;
-
-border-radius: 10px;
-padding: 15px;
-font-size: 15px;
-line-height: 20px;
-`;
-
-const CommentText = styled.p `
+const CommentText = styled.p`
   /* overflow: hidden; */
   display: inline-block;
   word-wrap: normal;
@@ -319,73 +274,66 @@ const CommentText = styled.p `
   color: var(--darkmode-color);
 `;
 
-const CommentButtonContainer = styled.div `
-display: flex;
-flex-direction: row;
-align-items: center;
-justify-content: end;
-gap: 8px;
+const CommentButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: end;
+  gap: 8px;
 `;
 
-const CommentEdit = styled.button `
- 
+const CommentEdit = styled.button`
+  border-radius: 5px;
 
- border-radius: 5px;
-
- width: 40px;
-height: 24px;
-border: 0.1px solid var(--button-border-color);
-border-radius: 5px;
-background-color: var(--darkmode-bgColor);
-&:hover {
-  background-color: ${(props) => props.theme.hoverColor};
-
-
-}
+  width: 40px;
+  height: 24px;
+  border: 0.1px solid var(--button-border-color);
+  border-radius: 5px;
+  background-color: var(--darkmode-bgColor);
+  &:hover {
+    background-color: ${(props) => props.theme.hoverColor};
+  }
 `;
-const CommentDelete = styled.button `
-border-radius: 5px;
+const CommentDelete = styled.button`
+  border-radius: 5px;
 
-width: 40px;
-height: 24px;
-border: 0.1px solid var(--button-border-color);
-border-radius: 5px;
-background-color: var(--darkmode-bgColor);
-&:hover {
- background-color: ${(props) => props.theme.hoverColor};
-
-
-}
+  width: 40px;
+  height: 24px;
+  border: 0.1px solid var(--button-border-color);
+  border-radius: 5px;
+  background-color: var(--darkmode-bgColor);
+  &:hover {
+    background-color: ${(props) => props.theme.hoverColor};
+  }
 `;
 
-const CommentButtonText = styled.p `
+const CommentButtonText = styled.p`
   color: var(--darkmode-color);
- flex-basis: 1;
- white-space: nowrap;
+  flex-basis: 1;
+  white-space: nowrap;
 `;
 
-const CommenterBox = styled.div `
+const CommenterBox = styled.div`
   display: flex;
   flex-direction: row;
 `;
 
-const UpdatedAt = styled.p `
+const UpdatedAt = styled.p`
   color: var(--darkmode-color);
   font-size: 10px;
 `;
 
-const EditInput = styled.input `
-
-background-color: transparent;
-border: 0.1px solid var(--button-border-color);
-color: var(--darkmode-color);
-width: 100%;
-margin-left: 25px;
-margin-right: 25px;
-margin-bottom: 15px;
+const EditInput = styled.input`
+  background-color: transparent;
+  border: 0.1px solid var(--button-border-color);
+  color: var(--darkmode-color);
+  width: 100%;
+  margin-left: 25px;
+  margin-right: 25px;
+  margin-bottom: 15px;
 `;
 
-const CommentListWrapper = styled.div `
+const CommentListWrapper = styled.div`
   width: 100%;
   height: 300px;
   overflow-y: scroll;
@@ -407,11 +355,8 @@ const CommentListWrapper = styled.div `
   }
 
   @media ${(props) => props.theme.tablet} {
-    height: calc(100vh - 400px);
   }
   @media ${(props) => props.theme.laptop} {
     overflow-y: visible;
-    
   }
-
 `;
